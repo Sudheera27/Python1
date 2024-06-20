9bbe47b6a08d490468a2f2bddb3d04aa5e56cfbb
Sudheera27 <sudheerapolu.sangamone@gmail.com>
2024-06-20 16:55:59 -0700

    Created a Password Generator

---
+++ 32.py
@@ -0,0 +1,7 @@
+import random
+rand1=random.randint(0,9)
+rand2=random.randint(10,99)
+rand4=random.randint(1000,9999)
+print(rand1)
+print(rand2)
+print(rand4)
---
+++ 34.py
@@ -0,0 +1,2 @@
+import mod_genpwd as pw
+pw.genpwd2(12)
